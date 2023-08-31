1b98cf73a7f0e92a2a7fd0662aa959e27eea2be4
Pauli <pauli_feu@yahoo.de>
2023-08-31 20:05:52 +0200

    updated astro to v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://paulda.net',
-    experimental: {
-        viewTransitions: true,
-    },
+    site: 'https://paulda.net/',
+   
 });
