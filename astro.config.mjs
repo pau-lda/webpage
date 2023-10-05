fde5ac1f974a2741224a3fb6fa8f1af2c1b03d10
Pauli <pauli_feu@yahoo.de>
2023-10-05 20:59:25 +0200

    Revert "Revert "added prefetch for transitionapi fallback""
    This reverts commit 6998941c09366dd763d69dadea2bc4bd37483807.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://paulda.net/',
-    integrations: [
-    ]
+  site: 'https://paulda.net/',
+  integrations: [prefetch()]
 });
